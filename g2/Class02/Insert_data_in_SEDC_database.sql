e6249ce0c7d7a651f58c3bc2e4079025c046e39b
Danilo Borozan <daniloborozan07@gmail.com>
2024-05-16 18:20:46 +0200

    add

--- g2/Class02/Insert_data_in_SEDC_database.sql
+++ g2/Class02/Insert_data_in_SEDC_database.sql
@@ -2,38 +2,38 @@
 ------------------
 
 delete from dbo.OrderDetails where 1=1;
-delete from dbo.[Orders] where 1=1;
-delete from dbo.BusinessEntities where 1=1;
-delete from dbo.Employees where 1=1;
-delete from dbo.Products where 1=1;
-delete from dbo.Customers where 1=1;
+delete from dbo.[Order] where 1=1;
+delete from dbo.BusinessEntity where 1=1;
+delete from dbo.Employee where 1=1;
+delete from dbo.Product where 1=1;
+delete from dbo.Customer where 1=1;
 GO
 
-insert into dbo.BusinessEntities (Name,Region,Zipcode,Size)
+insert into dbo.BusinessEntity (Name,Region,Zipcode,Size)
 values ('Vitalia Skopje','Skopski','1000','Large')
 GO
 
-insert into dbo.BusinessEntities (Name,Region,Zipcode,Size)
+insert into dbo.BusinessEntity (Name,Region,Zipcode,Size)
 values ('Vitalia Bitola','Bitolski','2000','Large')
 GO
 
-insert into dbo.BusinessEntities (Name,Region,Zipcode,Size)
+insert into dbo.BusinessEntity (Name,Region,Zipcode,Size)
 values ('Vitalia Strumica','Strumicki','987','Medium')
 GO
 
-insert into dbo.BusinessEntities (Name,Region,Zipcode,Size)
+insert into dbo.BusinessEntity (Name,Region,Zipcode,Size)
 values ('Vitalia Kumanovo','Kumanovski','500','Medium')
 GO
 
-insert into dbo.BusinessEntities (Name,Region,Zipcode,Size)
+insert into dbo.BusinessEntity (Name,Region,Zipcode,Size)
 values ('Vitalia Tetovo','Polog','700','Small')
 GO
 
-insert into dbo.BusinessEntities (Name,Region,Zipcode,Size)
+insert into dbo.BusinessEntity (Name,Region,Zipcode,Size)
 values ('Vitalia Stip','Stipski','300','Small')
 GO
 
-insert into dbo.BusinessEntities (Name,Region,Zipcode,Size)
+insert into dbo.BusinessEntity (Name,Region,Zipcode,Size)
 values ('Vitalia Ohrid','Ohridski','100','Small')
 GO
 
@@ -46,7 +46,7 @@ insert into @FirstName values ('Aleksandar'),('Ana'),('Borce'),('Biljana'),('Vik
 declare @LastName table (LastName nvarchar(50))
 insert into @LastName values ('Stojanovski'),('Nikolovski'),('Zdravevski'),('Ivanovski'),('Popovski'),('Mitrevski'),('Stoilov'),('Pandev'),('Todorov'),('Trajanov')
 
-insert into dbo.Employees(FirstName,LastName,DateOfBirth,Gender,HireDate,NationalIdNumber)
+insert into dbo.Employee(FirstName,LastName,DateOfBirth,Gender,HireDate,NationalIdNumber)
 select f.FirstName, l.LastName,'1980.01.01' as date, case when FirstName in ('Ana','Biljana','Valentina','Gordana','Maja') then 'F' else 'M' end as Gender,'2015.01.01' as HireDate,1 as IdNumber
 from @FirstName f
 CROSS JOIN @LastName l
@@ -55,7 +55,7 @@ GO
 update e set DateOfBirth = dateadd(MM,Id,DateOfBirth),  
 			 HireDate = dateadd(MM,2*Id,'1990.01.01'), 
 			 NationalIdNumber =  id + cast(10000000 * rand(id*10) as int)
-from dbo.Employees e
+from dbo.Employee e
 GO
 
 
@@ -64,7 +64,7 @@ GO
 declare @City table (City nvarchar(100),Region nvarchar(100))
 insert into @City
 select substring(name,9,100),Region 
-from dbo.BusinessEntities
+from dbo.BusinessEntity
 UNION select 'Radovis','Strumicki'
 UNION select 'Kocani','Stipski'
 UNION select 'Gevgelija','Strumicki'
@@ -72,22 +72,22 @@ UNION select 'Gevgelija','Strumicki'
 declare @Market table (name nvarchar(100))
 insert into @Market values ('Vero'),('Tinex'),('Ramstor'),('Kam'),('Zito'),('Zegin'),('Eurofarm'),('Makpetrol'),('Lukoil'),('Tobacco')
 
-insert into dbo.Customers (Name, City,RegionName, isActive)
+insert into dbo.Customer (Name, City,RegionName, isActive)
 select m.name + N' ' + c.City as Name, c.City as City, case when c.Region = 'Strumicki' then 'Stipski' else c.Region end as Region, 1 as IsActive
 from @Market m
 CROSS APPLY @City c
 
 update c set AccountNumber = id + cast(233255 * rand(id*7) as int),
-CustomerSize = case when id%3 = 0 then 'Small' when id % 3 = 1 then 'Medium' else 'Large' end, 
+Customerize = case when id%3 = 0 then 'Small' when id % 3 = 1 then 'Medium' else 'Large' end, 
 PhoneNumber = N'070' + Cast(cast(1000000 * rand(id*3) as int) as nvarchar(10))
- from dbo.Customers c
+ from dbo.Customer c
 
  GO
 
 -- Product
 -----------
 
-INSERT INTO dbo.Products (Name)
+INSERT INTO dbo.Product (Name)
 VALUES('Crunchy'),('Granola'),('Regular/soft'),('Gluten Free'),('Multigrain'),('Take away'),('Cornflakes'),('Cereals'),('Protein Bar'),('Raw bars'),('No Sugar Added'),('Multigrain'),
 ('Gluten Free'),('Kids Bar'),('Fruit, Nuts & Seeds'),('Protein balls'),('Rice Cakes'),('Mini cereal bites'),('Dried Fruits'),('Crackers'),('Cookies'),('Unrefined sugars'),
 ('Natural Sweeteners'),('Diabetic'),('Plant-based drinks'),('Smoothie drinks'),('Natural Juices'),('Green Teas'),('Functional Teas'),('Fruit Teas'),('Seeds'),('Oils & Vinegars'),
@@ -99,31 +99,31 @@ Cost = cast(20 + 10* id * rand(23+id) as int),
 Price = 5 + id + cast(20 + 10* id * rand(23+id) as int),
 Code = substring(Name,1,3) + cast(id as nvarchar(10)),
 Description = Name + ' description'
-from dbo.Products p
+from dbo.Product p
 GO
 
 -- Order
 --------
  
- Insert into dbo.[Orders] (OrderDate, Status, BusinessEntityId, CustomerId,EmployeeId,TotalPrice,Comment)
+ Insert into dbo.[Order] (OrderDate, Status, BusinessEntityId, CustomerId,EmployeeId,TotalPrice,Comment)
  select '2019.05.01' as OrderDate, 0 as Status, b.Id , c.Id, e.Id, 0 as TotalPrice, N'' as comment
- from dbo.BusinessEntities b
- cross apply dbo.Customers c
- cross apply dbo.Employees e
+ from dbo.BusinessEntity b
+ cross apply dbo.Customer c
+ cross apply dbo.Employee e
  where b.Id not in (13,15)
  and c.Id % 10 >3
  and e.Id % 10 <7
  and b.Region = c.RegionName
 
  update o set OrderDate =  dateadd(DD,-(Id%120),OrderDate)
- from dbo.[Orders] o
+ from dbo.[Order] o
  GO
 
  -- Order details
  INSERT INTO dbo.OrderDetails (OrderId, ProductId,Quantity,Price)
  select o.id,p.Id, 1 + (p.id + o.id) % 13,p.Price
- from [Orders] o
- cross apply Products p 
+ from [Order] o
+ cross apply Product p 
  where (o.id %2 + o.id %3 )= p.id % 11
  and p.id % 10 not in (3,4)
  and o.id % 10 < 7
@@ -131,20 +131,20 @@ GO
 
  ; with cte as (
  select o.Id, sum(d.Quantity * d.Price) as TotalPrice
- from dbo.[Orders] o
+ from dbo.[Order] o
  inner join dbo.OrderDetails d on o.Id = d.OrderId
  group by o.id 
  )
  update o set TotalPrice = c.TotalPrice
- from dbo.[Orders] o
+ from dbo.[Order] o
  inner join cte c on c.Id = o.id
 
 GO
 
 
-SELECT * from dbo.BusinessEntities
-select * from dbo.Employees
-SELECT * from dbo.Products
-select * from dbo.Customers 
-select * from dbo.[Orders]
+SELECT * from dbo.BusinessEntity
+select * from dbo.Employee
+SELECT * from dbo.Product
+select * from dbo.Customer 
+select * from dbo.[Order]
 select * from dbo.[OrderDetails]
